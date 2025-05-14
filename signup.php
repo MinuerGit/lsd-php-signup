520eb416017acf800c60f57aa9f1497ca23d0cf9
MinuerGit <minuer2003@gmail.com>
2025-05-14 10:44:26 +0100

    Final Changes

--- signup.php
+++ signup.php
@@ -2,41 +2,62 @@
 
 $message = "";
 $msgType = "success";
+$targetFile = "https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png";
+echo hash("sha256", "Micro0games");
 
 if (isset($_POST["signup"])){
-print_r($_POST);
+    print_r($_POST);
 
-require('config.php');
+    require('config.php');
 
-$name = $_POST ["name"];
-$email = $_POST ["email"];
-$pwd = $_POST ["password"];
+    $name = $_POST ["name"];
+    $email = $_POST ["email"];
+    $salt = "$#%_223!as";
+    $pwd = $_POST ["password"];
+    $strongPwd = hash("sha256", $pwd.$salt);
+    
 
-$query = "select iduser from user where email = '$email'";
-$emailResult = mysqli_query($connection, $query);
-$targetFile = "https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png";
+    $query = "select iduser from user where email = '$email'";
+    $emailResult = mysqli_query($connection, $query);
 
 
 
-if (mysqli_num_rows($emailResult) == 0) {
-    $message = "User registado";
 
-    $query = "insert into user (name, email, password) values ('$name', '$email', '$pwd', '$targetFile')";
+    if (mysqli_num_rows($emailResult) == 0) {
+        //se o email não está registado
 
-    mysqli_query($connection,$query);
+        $message = "User registado";
 
-    echo $query;
-} else {
-    $message = "Email já se encontra registado";
-    $msgType = "danger";
-};
 
-// avatar 
 
-if (isset($_FILES['image']) && $_FILES['image']['tmp_name'] !== "") {
-    $targetFile = "./images/".uniqid().basename($_FILES["image"]["name"]);
-    move_uploaded_file($_FILES['image']['tmp_name'], $targetFile);
-};
+        if (isset($_FILES['image']) && $_FILES['image']['tmp_name'] !== "") {
+
+            // verificar o tamanho da imagem
+            $size = getimagesize($_FILES['image']['tmp_name']);
+
+            if ($size) {
+                
+                $targetFile = "./images/".uniqid().basename($_FILES["image"]["name"]);
+                move_uploaded_file($_FILES['image']['tmp_name'], $targetFile);
+                $query = "insert into user (name, email, password, avatar) values ('$name', '$email', '$strongPwd', '$targetFile')";
+
+                mysqli_query($connection,$query);
+        
+                echo $query;
+            } else {
+                $message = "Imagem não suportada";
+                $msgType = "danger";
+            }
+
+        };
+
+    } else {
+        $message = "Email já se encontra registado";
+        $msgType = "danger";
+    };
+
+    // avatar 
+
 
 
 
@@ -73,7 +94,7 @@ if (isset($_FILES['image']) && $_FILES['image']['tmp_name'] !== "") {
 
         <form method="post" enctype="multipart/form-data">
 
-            <img src="<?php echo $targetFile ?>" class="img-thumbnail w-25" alt="...">
+            <img src="<?php echo $targetFile; ?>" class="img-thumbnail w-25" alt="...">
             <div class="mb-3">
                 <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
                 <input class="form-control" type="file" name="image" id="image">
@@ -81,11 +102,11 @@ if (isset($_FILES['image']) && $_FILES['image']['tmp_name'] !== "") {
 
             <div class="mb-3">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" value="">
+                <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" value="<?php if (isset($name)) echo $name; ?>">
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value="">
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value="<?php if (isset($email)) echo $email; ?>">
             </div>
 
             <div class="mb-3">
